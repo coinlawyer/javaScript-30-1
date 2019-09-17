4989e7f091cc6b4b24745dbbc0e7e294ddede4c8
unknown <achyryk86@gmail.com>
2019-09-18 00:39:48 +0300

    webcam video capture settled

---
+++ javaScript-30-19/js/index.js
@@ -0,0 +1,31 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const context = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo () {
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(err=> {
+            console.error('Oh NO!', err);   
+        });
+}
+
+function paintToCanvas () {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    console.log(width, height);
+    [canvas.width, canvas.height] = [width, height];
+
+    setInterval(() => {
+        context.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+getVideo();
+paintToCanvas();
