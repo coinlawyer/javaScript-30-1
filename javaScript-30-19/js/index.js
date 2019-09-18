39f410492436c6f34bfb0a6eb36d1f777a366936
unknown <achyryk86@gmail.com>
2019-09-19 01:04:36 +0300

    added flters

--- javaScript-30-19/js/index.js
+++ javaScript-30-19/js/index.js
@@ -7,7 +7,6 @@ const snap = document.querySelector('.snap');
 function getVideo () {
     navigator.mediaDevices.getUserMedia({video: true, audio: false})
         .then(localMediaStream => {
-            console.log(localMediaStream);
             video.srcObject = localMediaStream;
             video.play();
         })
@@ -19,23 +18,63 @@ function getVideo () {
 function paintToCanvas () {
     const width = video.videoWidth;
     const height = video.videoHeight;
-    console.log(width, height);
     [canvas.width, canvas.height] = [width, height];
 
     return setInterval(() => {
         context.drawImage(video, 0, 0, width, height);
+        // take the pixels out
         let pixels = context.getImageData(0, 0, width, height);
-        pixels = redEffect(pixels);
+        // mess them
+       //pixels = redEffect(pixels);
+    //    pixels = rgbSplit(pixels);
+        pixels = greenScreen(pixels);  
+    // pull them back
+        context.putImageData(pixels, 0, 0);    
+        // context.globalAlpha = 0.5;
     }, 16);
 }
 
 function redEffect (pixels) {
-    for (let i = 0; i < pixels.length; i+=4) {
-        pexels[i + 0] = pixels.data[i + 0] + 100;// red
-        pixels[i + 1] = pixels.data[i + 1] -50;// green
-        pixels[i + 2] = pixels.data[i + 2] * 0.5 // blue
-
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i + 0] -= 100;// red
+        pixels.data[i + 1] -= 50;// green
+        pixels.data[i + 2] *= 0.5; // blue
     }
+    return pixels;
+}
+
+function rgbSplit (pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data[i + 0];
+        pixels.data[i + 500] = pixels.data[i + 1];
+        pixels.data[i - 550] = pixels.data[i + 2];
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+    document.querySelectorAll('.rgb input').forEach((input)=>{
+        levels[input.name] = input.value;
+    });
+
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+    
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+                // take it out
+            pixels.data[i + 3] = 0;
+            }
+    }
+    return pixels;
 }
 
 function takePhoto () {
